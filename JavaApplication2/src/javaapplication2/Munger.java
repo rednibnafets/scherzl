25e60e5320b623f1ec52d0b7f4e77d531bc76896
stefan <stefan@silverdrop.trillium.lan>
2015-12-01 21:59:22 +0000

    02 - add the Widget class

---
+++ JavaApplication2/src/javaapplication2/Munger.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author s.binder
+ */
+public class Munger {
+    
+    private ArrayList< Integer > al ;
+    
+    public Munger() 
+    {
+        for(Integer i : al )
+        {
+            al.add(i) ;
+        }
+    }
+    
+}
