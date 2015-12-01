ce588fed969380dcb38568ad50fa6632c7906de6
stefan <stefan@silverdrop.trillium.lan>
2015-12-01 21:25:47 +0000

    02 - add the Widget class

---
+++ src/javaapplication007/Widget.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication007;
+
+/**
+ *
+ * @author stefan
+ */
+public class Widget {
+    
+    private int internal_ ;
+    private int external_ ;
+    
+    public Widget(){
+       this.internal_ = 0 ;
+       this.external_ = 0 ;
+    }
+    
+}
